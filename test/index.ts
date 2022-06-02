37a3495d008e28ed694dd623555d3dbbecce5c4d
SmartCoding51 <topstar51@outlook.com>
2022-06-02 17:34:56 +0900

    update unit tests for passport contract

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,69 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+import { Passport } from '../typechain';
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Passport NFT", function () {
+  let passportContract: Passport;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  before(async () => {
+    const PassportFactory = await ethers.getContractFactory("Passport");
+    passportContract = await PassportFactory.deploy(
+      [
+        "Amethyst",
+        "Aquamarine",
+        "Bronze",
+        "Copper",
+        "Emerald",
+        "Garnet",
+        "Gold",
+        "Granite",
+        "Limited",
+        "Neon",
+        "Ruby",
+        "Silver",
+      ], // Names
+      [
+        "General",
+        "General",
+        "Diplomat",
+        "General",
+        "General",
+        "General",
+        "VIP",
+        "General",
+        "VIP",
+        "Diplomat",
+        "Diplomat",
+        "Diplomat",
+      ], // Class
+      [
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/amethyst.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/aquamarine.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/bronze.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/copper.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/emerald.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/garnet.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/gold.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/granite.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/limited.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/neon.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/ruby.png",
+        "https://ipfs.io/ipfs/QmP3caLHzPRdsXD6Azot8Rf5SmL4Eck6q8NwBk6nNVypZq/silver.png",
+      ]
+    );
+    await passportContract.deployed();
+    console.log("Contract deployed to:", passportContract.address);
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("Mint the first passport nft", async function () {
+    let txn;
+    let returnedTokenUri;
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    txn = await passportContract.mintPassportNFT(0);
+    await txn.wait();
+
+    returnedTokenUri = await passportContract.tokenURI(1);
+    console.log("Minted Passport NFT #1\nURI:", returnedTokenUri);
   });
 });
